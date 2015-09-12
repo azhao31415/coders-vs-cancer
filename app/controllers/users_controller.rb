cbda9f6e19f2b7eb9806cd13e5a54c0b5e886dca
Sheldon Martin <sheld0ri@gmail.com>
2015-09-12 15:46:30 -0400

    Users#show now sets user_id based on params temporarily
    This change has Users#show get user_id from params
    instead of session, until user login is complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(session[:user_id])  
+    # @user = User.find(session[:user_id])  
+    @user = User.find(params[:id])
   end
 
   def new
