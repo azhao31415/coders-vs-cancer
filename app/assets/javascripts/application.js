dd0defc8c0e2b12bdd2b1915d7efa6b263a20046
AlinaJahnes <alina.jahnes@gamil.com>
2015-09-12 13:04:06 -0400

    add js for user login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,63 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+var signup, login, logout
+login = function(response) {
+	if (response.status == "failed") {
+		$('#login_warning').show();
+	} else if (response.status == "created") {
+		$('#login_warning').hide();
+		$('#user_profile_link').text(response["name"]);
+		$('.user_links').toggle();
+		$("#account_modal").foundation('reveal', 'close');
+	};
+};
+
+logout = function() {
+	$.ajax({
+		url: "/logout",
+		dataType: "json",
+	}).done(function(saveMessage){
+		$('#user_profile_link').text("no user");
+		$('.user_links').toggle();
+	}).fail(function(saveMessage){
+	 	console.log("logout failed");
+	});
+};
+
+signup = function(response) {
+	$('#user_profile_link').text(response["name"]);
+	$('.user_links').toggle();
+	$("#account_modal").foundation('reveal', 'close');
+};
+
+accountModalOpen = function(type) {
+	$("#account_modal").foundation('reveal', 'open');
+	accountModalTypeSelect(type);
+};
+
+accountModalTypeSelect = function(type) {
+	if (type === "login") {
+		$("#account_modal_login").show();
+		$("#account_modal_signup").hide();
+	} else if (type === "signup") {
+		$("#account_modal_login").hide();
+		$("#account_modal_signup").show();
+	};
+};
+
+$(document).on('ready page:load', function(){
+	$(document).on('click', '#logout_link', function(event){
+		event.preventDefault();
+		logout();
+	});
+	$(document).on('click', '#login_link', function(event){
+		event.preventDefault();
+		accountModalOpen("login");
+	});
+	$(document).on('click', '#signup_link', function(event){
+		event.preventDefault();
+		accountModalOpen("signup");
+	});
+
+});
