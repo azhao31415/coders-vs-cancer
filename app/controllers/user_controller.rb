de6f3a4e2902295a0f33113fe6fadd3ee7acaf8a
Sheldon Martin <sheld0ri@gmail.com>
2015-09-11 22:29:42 -0400

    Adds show and new methods to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,13 @@
 class UserController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
   private
 
   def set_user
