76f979c7f71401cfbbc9500ec821cf08e9370be8
Alina Jahnes <alina.jahnes@gmail.com>
2015-09-11 21:16:00 -0400

    add columns for user table

--- db/migrate/20150912004702_user.rb
+++ db/migrate/20150912004702_user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :email_verify, null: false
+      t.string :phone, null: false
+      t.string :phone_verify, default: nil
+      t.string :phone_provider, null: false
+      t.string :prefered_contact, defaflt: nil
+      t.string :password_digest, null: false
+      t.date  :check_day_of_month, null: false
+      t.timestamps
+    end
   end
 end
