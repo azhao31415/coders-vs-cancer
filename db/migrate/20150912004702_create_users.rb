e67399db3a75e3277a15fe57cc985ea6f3d52c7a
Sheldon Martin <sheld0ri@gmail.com>
2015-09-11 23:38:37 -0400

    Changes check_day_of_month field to an integer

--- db/migrate/20150912004702_create_users.rb
+++ db/migrate/20150912004702_create_users.rb
@@ -10,7 +10,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :phone_provider, null: false
       t.string :prefered_contact, default: nil
       t.string :password_digest, null: false
-      t.date  :check_day_of_month, null: false
+      t.integer :check_day_of_month, null: false
       t.timestamps
     end
   end
