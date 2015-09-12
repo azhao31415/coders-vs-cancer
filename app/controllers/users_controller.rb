32d1b2e5e236270b919d541a3b897c9d6a846a05
AlinaJahnes <alina.jahnes@gamil.com>
2015-09-12 18:44:56 -0400

    fixing merge conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user
-    else
-      render :new
-    end
+		respond_to do |format| 
+	  	if @user.save        
+		 		session[:user_id] = @user.id    
+		  	format.html { redirect_to root_path, notice: 'Thanks for signing up.' }
+				format.json { render json: {status: :created, name: @user.first_name} }
+			else
+				format.html { render :new }
+				format.json { render json: {status: "failed to create account"} }
+			end
+		end
   end
 
   private
