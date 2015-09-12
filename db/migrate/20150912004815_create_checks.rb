7ee47e4bf8a8bce04df8433f4f67b302606b36a4
Alina Jahnes <alina.jahnes@gmail.com>
2015-09-11 21:42:49 -0400

    fix typo

--- db/migrate/20150912004815_create_checks.rb
+++ db/migrate/20150912004815_create_checks.rb
@@ -1,7 +1,7 @@
 class CreateChecks < ActiveRecord::Migration
   def change
     create_table :checks do |t|
-      t.string :status, default: "unfufilled"
+      t.string :status, default: "unfulfilled"
       t.integer :user_id, null: false
       t.timestamps
     end
